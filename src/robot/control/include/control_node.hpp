1fac76675adf77076c9ce6fa5917a57781d27c6f
Vishalnar26 <Vishalrayan2608@gmail.com>
2026-09-23 00:18:54 -0400

    Implement control node pure pursuit logic

--- src/robot/control/include/control_node.hpp
+++ src/robot/control/include/control_node.hpp
@@ -5,12 +5,38 @@
 
 #include "control_core.hpp"
 
+#include "nav_msgs/msg/path.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include <optional>
+
 class ControlNode : public rclcpp::Node {
   public:
     ControlNode();
 
   private:
     robot::ControlCore control_;
+    void pathCallback(const nav_msgs::msg::Path::SharedPtr msg);
+    void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    void controlLoop();
+
+    rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_vel_pub_;
+    rclcpp::TimerBase::SharedPtr control_timer_;
+
+    nav_msgs::msg::Path::SharedPtr current_path_;
+    nav_msgs::msg::Odometry::SharedPtr robot_odom_;
+
+    std::optional<geometry_msgs::msg::PoseStamped> findLookaheadPoint();
+    geometry_msgs::msg::Twist computeVelocity(const geometry_msgs::msg::PoseStamped &target);
+    double computeDistance(const geometry_msgs::msg::Point &a, const geometry_msgs::msg::Point &b);
+    double extractYaw(const geometry_msgs::msg::Quaternion &quat);
+
+    double lookahead_distance_ = 1.0;
+    double goal_tolerance_ = 0.1;
+    double linear_speed_ = 0.5;
+    
 };
 
 #endif
