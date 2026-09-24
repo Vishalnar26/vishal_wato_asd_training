49fad0b94db2839f7eb41cdd5ca9acd6acca027a
azazazazah <zhongashley3@gmail.com>
2026-09-23 20:14:21 -0400

    clean up planner header

--- src/robot/planner/include/planner_core.hpp
+++ src/robot/planner/include/planner_core.hpp
@@ -3,21 +3,6 @@
 
 #include "rclcpp/rclcpp.hpp"
 
-namespace robot
-{
-
-class PlannerCore {
-  public:
-    explicit PlannerCore(const rclcpp::Logger& logger);
-
-  private:
-    rclcpp::Logger logger_;
-};
-#ifndef PLANNER_CORE_HPP_
-#define PLANNER_CORE_HPP_
-
-#include "rclcpp/rclcpp.hpp"
-
 // used for storing and comparing A* cells
 #include <functional>
 #include <cstddef>
@@ -29,73 +14,66 @@ class PlannerCore {
 
 namespace robot
 {
-  // represents one cell in the map grid
-  // 2d grid index
-  struct CellIndex {
-    int x;
-    int y;
 
-    CellIndex(int xx, int yy) : x(xx), y(yy) {}
+// represents one cell in the map grid
+struct CellIndex {
+  int x;
+  int y;
 
-    CellIndex() : x(0), y(0) {}
+  CellIndex(int xx, int yy) : x(xx), y(yy) {}
 
-    // checks if two cells are the same
-    bool operator==(const CellIndex& other) const {
-      return x == other.x && y == other.y;
-    }
+  CellIndex() : x(0), y(0) {}
 
-    // checks if two cells are different
-    bool operator!=(const CellIndex& other) const {
-      return x != other.x || y != other.y;
-    }
-  };
+  // checks if two cells are the same
+  bool operator==(const CellIndex& other) const {
+    return x == other.x && y == other.y;
+  }
 
-  // lets grid cells be stored using their x and y values
-  // "Hash function for CellIndex so it can be used in std::unordered_map"
-  struct CellIndexHash {
-    std::size_t operator()(const CellIndex& cell) const {
-      // A simple hash combining x and y
-      return std::hash<int>()(cell.x) ^ (std::hash<int>()(cell.y) << 1);
-    }
-  };
+  // checks if two cells are different
+  bool operator!=(const CellIndex& other) const {
+    return x != other.x || y != other.y;
+  }
+};
 
-  // stores a cell and its A* score
-  // "Structure representing a node in the A* open set"
-  struct AStarNode {
-    CellIndex index;
-    double f_score;
+// lets grid cells be stored in unordered maps
+struct CellIndexHash {
+  std::size_t operator()(const CellIndex& cell) const {
+    return std::hash<int>()(cell.x) ^
+           (std::hash<int>()(cell.y) << 1);
+  }
+};
 
-    AStarNode(CellIndex idx, double f)
-        : index(idx), f_score(f) {}
-  };
+// stores a cell and its A* score
+struct AStarNode {
+  CellIndex index;
+  double f_score;
 
-  // compares A* nodes by their f score
-  // "Comparator for the priority queue (min-heap by f_score)"
-  struct CompareF {
-    bool operator()(const AStarNode& a, const AStarNode& b) const {
-      // "We want the node with the smallest f_score on top"
-      return a.f_score > b.f_score;
-    }
-  };
+  AStarNode(CellIndex idx, double f)
+      : index(idx), f_score(f) {}
+};
 
-  class PlannerCore {
-    public:
-      explicit PlannerCore(const rclcpp::Logger& logger);
-      // finds a path from the robot to the goal
-      nav_msgs::msg::Path findPath(
-          const nav_msgs::msg::OccupancyGrid& map,
-          const geometry_msgs::msg::Point& start,
-          const geometry_msgs::msg::Point& goal
-      );
+// compares A* nodes by their f score
+struct CompareF {
+  bool operator()(const AStarNode& a, const AStarNode& b) const {
+    return a.f_score > b.f_score;
+  }
+};
 
-    private:
-      rclcpp::Logger logger_;
-  };
+class PlannerCore {
+  public:
+    explicit PlannerCore(const rclcpp::Logger& logger);
 
-}  
+    // finds a path from the robot to the goal
+    nav_msgs::msg::Path findPath(
+        const nav_msgs::msg::OccupancyGrid& map,
+        const geometry_msgs::msg::Point& start,
+        const geometry_msgs::msg::Point& goal
+    );
 
-#endif  
+  private:
+    rclcpp::Logger logger_;
+};
 
-}  
+}
 
-#endif  
+#endif
